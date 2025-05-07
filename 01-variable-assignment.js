7d45fb453fe2df40bdda172297d41147b0b95d48
Shinagare_swapnil <swapnilshinagare@gmail.com>
2025-05-07 08:47:58 +0530

    assignment of variable

---
+++ 01-variable-assignment.js
@@ -0,0 +1,14 @@
+var cityName="Hyderabad";
+console.log("My city name: ",cityName);
+
+var bestFriend="Leo";
+console.log("My Best friend name: ",bestFriend);
+
+var countryName="India";
+console.log("My Country name: ",countryName);
+
+var schoolName="MHM Highschool";
+console.log("My School name: ",schoolName);
+
+var collegeName="JSPM Pune";
+console.log("My college name: ",collegeName);
