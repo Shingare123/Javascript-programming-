0da6142e3e7b7c9c47fab806fd7b041635f76e61
Shinagare_swapnil <swapnilshinagare@gmail.com>
2025-05-11 22:43:30 +0530

    String

---
+++ assign.js
@@ -0,0 +1,14 @@
+function stringBasics() {
+    var dreamCompany="Google";
+    console.log(`My dream company is: ${dreamCompany}`);
+    var myHobby1="Travelling ";
+    var myHobby2=" Exploring new things ";
+    var myHobby3=" Riding bike";
+    console.log(`My hobbies are : ${myHobby1}+${myHobby2}+${myHobby3}`);
+    var sumOfTotalChar=myHobby1.length=myHobby2.length+myHobby3.length;
+    console.log(`Sum of total number of characters in hobbies:${sumOfTotalChar}`);
+    
+
+}
+stringBasics();
+
