159b234d35b2d58eb8dcb0235ef12c26eeff627d
Shinagare_swapnil <swapnilshinagare@gmail.com>
2025-05-15 09:30:32 +0530

    Assignments

--- 05-oepratorA.js
+++ 05-oepratorA.js
@@ -38,3 +38,4 @@ function StringLogicalProg(params) {
 
 }
 StringLogicalProg();
+
---
+++ 06-if-stmt.js
@@ -0,0 +1,10 @@
+console.log("============If statement==========");
+var num=0;
+if (num>0){
+    console.log(`No is greater than 0`);
+    
+}
+else{
+    console.log(`No is less than 0`);
+    
+}
